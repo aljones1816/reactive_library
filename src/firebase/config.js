74963ef557d09774a81bbc1c5ce6dacc4f7b5965
Alan Jones <aljones1816@gmail.com>
2021-11-21 09:35:23 -0500

    added firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBbnAvcdVkNjJRwbhi_76Y2Tkg_HZBHf84",
+    authDomain: "mybrary-d3fa1.firebaseapp.com",
+    projectId: "mybrary-d3fa1",
+    storageBucket: "mybrary-d3fa1.appspot.com",
+    messagingSenderId: "994855263871",
+    appId: "1:994855263871:web:2a9bf5e6fc60244906e94a"
+  };
+
+  // init firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //init services
+  const projectFirestore = firebase.firestore();
+
+  export { projectFirestore }
+  
